ef3e0e96ce26134ae288e7281da4173a7e83309a
ahmednasser1911 <ahmednasser.fcis@gmail.com>
2020-02-25 16:54:51 +0200

    Initial commit

---
+++ src/components/tracks/Tracks.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import {Consumer} from '../../context';
+import Spinner from '../layouts/Spinner'
+import Track from './Track';
+
+ class Tracks extends Component {
+    render() {
+        return (
+            <Consumer>
+                {value =>{
+                    const { track_list, headding } = value;
+                    if(track_list === undefined || track_list.length === 0)
+                        return <Spinner />
+                    else 
+                        return (
+                            <React.Fragment>
+                            <h3 className = "text-center mb-4">{headding}</h3>
+                            <div className = "row">
+                                {track_list.map(track => {
+                                    return <Track key={track.track.track_id} track= {track.track}/>
+                                })}
+                            </div>
+                            </React.Fragment>
+                        )
+                }
+                }
+            </Consumer>
+        )
+    }
+}
+export default Tracks
